75bb2a2941d81eaeaf43a27b717df04c68afd9c6
abhi1 <abhi1005raj@gmail.com>
2025-06-07 19:49:00 +0530

    swagger UI in production change

--- Program.cs
+++ Program.cs
@@ -6,7 +6,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
+app.UseSwagger();
+app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {

bed6d2eedd11c3879b9492b9c79c8dad1b8c22c5
abhi1 <abhi1005raj@gmail.com>
2025-06-08 21:04:43 +0530

    removed duplicacy

--- DotNet-Core-Github-Action/Program.cs
+++ DotNet-Core-Github-Action/Program.cs
@@ -9,11 +9,6 @@ var app = builder.Build();
 app.UseSwagger();
 app.UseSwaggerUI();
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
 
 app.UseHttpsRedirection();
 
--- DotNet-Core-Github-Action/obj/Debug/net8.0/API Github Actions.AssemblyInfo.cs
+++ DotNet-Core-Github-Action/obj/Debug/net8.0/API Github Actions.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API Github Actions")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f1f5115a52f68e39fadb8b5bb218c44c2ac80e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("API Github Actions")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API Github Actions")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
